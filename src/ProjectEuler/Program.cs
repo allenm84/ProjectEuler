b4083e1c4548924e322a4a1bf65f274e7632713a
Michael Allen <michael.allen@cognex.com>
2016-01-13 15:10:58 -0600

    Restart the whole thing. Use branches for each problem

--- src/ProjectEuler/Program.cs
+++ src/ProjectEuler/Program.cs
@@ -5,17 +5,11 @@ using System.Windows.Forms;
 
 namespace ProjectEuler
 {
-  internal static class Program
+  class Program
   {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    private static void Main()
+    static void Main(string[] args)
     {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new MainForm());
+
     }
   }
 }
