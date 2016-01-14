cb2495149ba04a2fdb6e4468f00bea2942016f1d
Michael Allen <michael.allen@cognex.com>
2016-01-14 13:13:28 -0600

    Solution to problem 12

---
+++ src/ProjectEuler/Problems/p0012.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Advanced;
+
+namespace ProjectEuler
+{
+  public class p0012 : euler
+  {
+    public override void Run()
+    {
+      var i = 11;
+      var factorCount = 0;
+
+      var result = 0;
+      while (factorCount < 500)
+      {
+        var triangle = Enumerable.Range(1, i).Sum();
+        factorCount = ntheory.divisors(triangle).Count();
+
+        result = triangle;
+        ++i;
+      }
+
+      Console.WriteLine(result);
+    }
+  }
+}
