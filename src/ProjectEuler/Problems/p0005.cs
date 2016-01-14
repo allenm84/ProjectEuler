7d2e38958d1ee0eda69d8d7e6a80ad58c65d0cb2
Michael Allen <michael.allen@cognex.com>
2016-01-14 10:38:40 -0600

    Solution to problem 5

---
+++ src/ProjectEuler/Problems/p0005.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Advanced;
+
+namespace ProjectEuler
+{
+  public class p0005 : euler
+  {
+    public override void Run()
+    {
+      var numbers = Enumerable.Range(1, 20).ToArray();
+
+      var result = 0;
+      var i = 20;
+
+      while (result == 0)
+      {
+        if (numbers.All(n => (i % n) == 0))
+        {
+          result = i;
+        }
+        ++i;
+      }
+
+      Console.WriteLine(result);
+    }
+  }
+}
