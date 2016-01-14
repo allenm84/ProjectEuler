53865670d21ab8cce3a0b1670b8b07e36bd993c8
Michael Allen <michael.allen@cognex.com>
2016-01-14 09:46:16 -0600

    Solution to problem 2

---
+++ src/ProjectEuler/Problems/p0002.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Advanced;
+
+namespace ProjectEuler
+{
+  public class p0002 : euler
+  {
+    const int FourMillion = 4000000;
+
+    public override void Run()
+    {
+      var fib1 = 1;
+      var fib2 = 2;
+
+      var sum = 2;
+      var keepGoing = true;
+
+      while (keepGoing)
+      {
+        var next = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = next;
+
+        keepGoing = next < FourMillion;
+        if (next % 2 == 0)
+        {
+          sum += next;
+        }
+      }
+
+      Console.WriteLine(sum);
+    }
+  }
+}
