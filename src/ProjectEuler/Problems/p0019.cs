32aceb82dfb296e7a7c1baab01ca791efbb50006
Michael Allen <michael.allen@cognex.com>
2016-01-14 14:39:10 -0600

    Solution to problem 19

---
+++ src/ProjectEuler/Problems/p0019.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Advanced;
+using System.Advanced.Combinations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+  public class p0019 : euler
+  {
+    public override void Run()
+    {
+      // 1 Jan 1901 to 31 Dec 2000
+      var count = 0;
+      var start = DateTime.Parse("1/1/1901");
+      var end = DateTime.Parse("12/31/2000");
+      for (var i = start; i <= end; i = i.AddDays(1))
+      {
+        if (i.Day == 1 && i.DayOfWeek == DayOfWeek.Sunday)
+        {
+          ++count;
+        }
+      }
+      Console.WriteLine(count);
+    }
+  }
+}
