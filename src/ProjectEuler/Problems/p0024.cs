86db4c2f8155743002fc1090a7258c1c2ad3d9ef
Michael Allen <michael.allen@cognex.com>
2016-01-14 15:16:34 -0600

    Solution to problem 24

---
+++ src/ProjectEuler/Problems/p0024.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Advanced;
+using System.Advanced.Combinations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+  public class p0024 : euler
+  {
+    public override void Run()
+    {
+      var digits = "0123456789";
+      var perm = new Permutations<char>(digits.ToList());
+      var values = perm
+        .Select(p => Convert.ToInt64(string.Join("", p)))
+        .OrderBy(i => i)
+        .ToArray();
+      Console.WriteLine(values[1000000 - 1]);
+    }
+  }
+}
