367442b3d7ea25da956fdd994c3fa4b6bb8686d5
Michael Allen <michael.allen@cognex.com>
2016-01-14 09:43:32 -0600

    Solution to problem 1

--- src/ProjectEuler/Problems/euler.cs
+++ src/ProjectEuler/Problems/euler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,29 @@ namespace ProjectEuler
 {
   public abstract class euler
   {
+    static Dictionary<int, Func<euler>> sFactoryCache = new Dictionary<int, Func<euler>>();
+
     public abstract void Run();
 
     public static void Solve<T>() where T : euler, new()
     {
       new T().Run();
+    }
+
+    public static void Solve(int number)
+    {
+      Func<euler> factory;
+
+      if (!sFactoryCache.TryGetValue(number, out factory))
+      {
+        var type = Type.GetType(string.Format("ProjectEuler.p{0:0000}", number));
+        var exp = Expression.New(type);
+        var lambda = LambdaExpression.Lambda<Func<euler>>(exp);
+        factory = lambda.Compile();
+        sFactoryCache[number] = factory;
+      }
+
+      factory().Run();
     }
   }
 }
---
+++ src/ProjectEuler/Problems/p0001.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Advanced;
+
+namespace ProjectEuler
+{
+  public class p0001 : euler
+  {
+    public override void Run()
+    {
+      Console.WriteLine(Enumerable
+        .Range(0, 1000)
+        .Where(i => (i % 5) == 0 || (i % 3) == 0)
+        .Sum());
+    }
+  }
+}
