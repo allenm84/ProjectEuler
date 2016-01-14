5ecec8a3631cf98bb12e22612156d58dc3213ef5
Michael Allen <michael.allen@cognex.com>
2016-01-14 14:42:05 -0600

    Solution to problem 20

---
+++ src/ProjectEuler/Problems/p0020.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Advanced;
+using System.Advanced.Combinations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+  public class p0020 : euler
+  {
+    public override void Run()
+    {
+      var prod = BigInteger.One;
+      for (var i = 100; i > 0; --i)
+      {
+        var n = new BigInteger(i);
+        prod *= n;
+      }
+
+      Console.WriteLine(prod
+        .ToString()
+        .Select(c => ((int)c) - 48)
+        .Sum());
+    }
+  }
+}
