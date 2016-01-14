dc0bc3a0e65e3c6305ea1ee040a9e0ad897fd75b
Michael Allen <michael.allen@cognex.com>
2016-01-14 10:41:43 -0600

    Solution to problem 6

---
+++ src/ProjectEuler/Problems/p0006.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Advanced;
+
+namespace ProjectEuler
+{
+  public class p0006 : euler
+  {
+    public override void Run()
+    {
+      long sumSquares = 0;
+      long sum = 0;
+
+      for (long i = 1; i < 101; ++i)
+      {
+        var square = (i * i);
+        sumSquares += square;
+        sum += i;
+      }
+
+      var squareSum = (sum * sum);
+      Console.WriteLine(Math.Abs(sumSquares - squareSum));
+    }
+  }
+}
