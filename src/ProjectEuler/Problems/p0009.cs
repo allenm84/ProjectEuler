93b08538a21f6092dab7b62ae489b6d7d4d43b09
Michael Allen <michael.allen@cognex.com>
2016-01-14 12:46:13 -0600

    Solution to problem 9

---
+++ src/ProjectEuler/Problems/p0009.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Advanced;
+
+namespace ProjectEuler
+{
+  public class p0009 : euler
+  {
+    public override void Run()
+    {
+      var squares = Enumerable.Range(0, 1000).Select(i => (i * i)).ToArray();
+      long result = 0;
+
+      for (var a = 1; a < 999; ++a)
+      {
+        var a2 = squares[a];
+        for (var b = a + 1; b < 999; ++b)
+        {
+          var b2 = squares[b];
+          for (var c = b + 1; c < 999; ++c)
+          {
+            var c2 = squares[c];
+
+            if ((a + b + c) != 1000) { continue; }
+            if ((a2 + b2) != c2) { continue; }
+
+            long al = a;
+            long bl = b;
+            long cl = c;
+            result = al * bl * cl;
+          }
+        }
+      }
+
+      Console.WriteLine(result);
+    }
+  }
+}
