fbd5aaf5ff0c5ef9029458f401ad3f1186eac600
Michael Allen <michael.allen@cognex.com>
2016-01-14 08:45:08 -0600

    [mod] Added base euler class [mod] Added nuget package

---
+++ src/ProjectEuler/Problems/euler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+  public abstract class euler
+  {
+    public abstract void Run();
+
+    public static void Solve<T>() where T : euler, new()
+    {
+      new T().Run();
+    }
+  }
+}
