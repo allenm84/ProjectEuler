2e5d013867321b44abadd4279c9dc3daf15796ef
Michael Allen <michael.allen@cognex.com>
2016-01-15 11:37:10 -0600

    Solution to problem 36

---
+++ src/ProjectEuler/Problems/Solved/p0036.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Advanced;
+using System.Advanced.Combinations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+  public class p0036 : euler
+  {
+    public override void Run()
+    {
+      long sum = 0;
+      for (var i = 1; i < 1000000; ++i)
+      {
+        if (math.isPalindrome(i))
+        {
+          var base02 = Convert.ToString(i, 2);
+          if (math.isPalindrome(base02))
+          {
+            sum += i;
+          }
+        }
+      }
+      Console.WriteLine(sum);
+    }
+  }
+}
