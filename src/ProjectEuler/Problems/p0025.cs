1b15bbeb5312f3ffc6f8652dfbf61039fee1c95e
Michael Allen <michael.allen@cognex.com>
2016-01-15 09:08:19 -0600

    Solution to problem 25

---
+++ src/ProjectEuler/Problems/p0025.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Advanced;
+using System.Advanced.Combinations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+  public class p0025 : euler
+  {
+    public override void Run()
+    {
+      var f1 = BigInteger.One;
+      var f2 = BigInteger.One;
+      var sum = BigInteger.Zero;
+
+      var keepGoing = true;
+      var n = 2;
+
+      while (keepGoing)
+      {
+        sum = f1 + f2;
+        f1 = f2;
+        f2 = sum;
+        ++n;
+
+        var text = sum.ToString();
+        if (text.Length == 1000)
+        {
+          keepGoing = false;
+        }
+      }
+
+      Console.WriteLine(n);
+    }
+  }
+}
